646ecec186d8f41e1fbf553c97b846dfc4004208
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-06-17 21:38:39 -0400

    validates Product & some change in the show.html, partial, etc

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :reference, presence: true
+    validates :description, presence: true, length: {minimum:10, maximum:100}
 end
